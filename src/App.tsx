59ab017ae9edbfb38e94d4a27c1da269fa6c483f
holfreij <holfreij@users.noreply.github.com>
2025-01-27 12:57:09 +0100

    Add team creation algorithm

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { createBalancedTeams, PlayerStats, TeamResults } from "./algorithm";
+import { useEffect, useState } from "react";
+
+const playerStats: PlayerStats[] = [
+  { strength: 5, handicap: 0, name: "Kevin" },
+  { strength: 4, handicap: 0, name: "Frank" },
+  { strength: 3.8, handicap: 0, name: "Thomas" },
+  { strength: 3.7, handicap: 0, name: "Maarten" },
+  { strength: 3.0, handicap: 0, name: "Rick" },
+  { strength: 3.1, handicap: 0, name: "Rolf" },
+  { strength: 2.9, handicap: 0, name: "Joel" },
+  { strength: 2.8, handicap: 0, name: "Lennard" },
+  { strength: 2.5, handicap: 0, name: "Guido" },
+  { strength: 2.0, handicap: 0, name: "Jan-Joost" },
+];
+const maxTeamStrengthDifference = 0.5;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [solutions, setSolutions] = useState<TeamResults[]>([]);
+
+  const onCreateTeams = () => {
+    setSolutions(createBalancedTeams(playerStats, maxTeamStrengthDifference));
+  };
+
+  useEffect(() => {
+    console.log("Solutions:");
+    solutions.forEach((solution, index) => {
+      console.log(`Solution ${index + 1}:`);
+      console.log(
+        "  Group 1:",
+        solution.team1.map((item) => item.name),
+        "Sum:",
+        solution.team1.reduce((a, b) => a + b.strength, 0)
+      );
+      console.log(
+        "  Group 2:",
+        solution.team2.map((item) => item.name),
+        "Sum:",
+        solution.team2.reduce((a, b) => a + b.strength, 0)
+      );
+      console.log("  Difference:", solution.strengthDifference);
+    });
+  }, [solutions]);
 
   return (
     <>
@@ -18,9 +58,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={onCreateTeams}>Maak teams</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -29,7 +67,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
