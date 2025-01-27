bb23be30dcc829ca3fc54fe5df6f22828e7aaa06
holfreij <holfreij@users.noreply.github.com>
2025-01-27 14:31:55 +0100

    Give ChakraUI a try

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/cod-teams/",
-  plugins: [tailwindcss(), react()],
+  plugins: [tailwindcss(), tsconfigPaths(), react()],
 });
